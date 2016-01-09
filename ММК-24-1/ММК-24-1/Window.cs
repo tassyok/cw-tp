e4d666e6ea3070eb11ea688a870162218a833b10
Таисия Иванова < >
2016-01-09 17:20:57 +0300

    implement class methods

--- ММК-24-1/ММК-24-1/Window.cs
+++ ММК-24-1/ММК-24-1/Window.cs
@@ -132,16 +132,45 @@ namespace MonteKarlo
             if (ru > 0)
             {
                 this.r = ru;
-                Square();
+               this.Sq = Square();
             }
             else throw new ArgumentException();
         }
     }
 
-    public static class Rectangle { }
+    public static class Rectangle {
+        float R;
+        Point D;
+        Point C;
+        float Sq;
+        public Rectangle(Point d, Point c, float r)
+        {
+            this.C = c;
+            this.D = d;
+            this.R = r;
+            this.Sq = Square();
+        } 
+        float Square()
+        {
+            return (D.X + R - C.X) * C.Y;
+        }
+    }
 
     public static class Triangle {
-    
+        Point D;
+        Point C;
+        float Sq;
+        public Triangle(Point d, Point c)
+        {
+            this.C = c;
+            this.D = d;
+            this.Sq = Square();
+        }
+
+        private float Square()
+        {
+            return ((D.X - (float)C.X) * C.Y);
+        }
     }
 
 }
