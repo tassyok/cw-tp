f51f4d12090ca43f89adcff7ac9aff9d90584322
Lokker <lokkersp@gmail.com>
2016-01-10 17:02:57 +0300

    fix client

--- wcf-part/MMK-24-3-client/MMK-24-3-client/Window.cs
+++ wcf-part/MMK-24-3-client/MMK-24-3-client/Window.cs
@@ -15,17 +15,22 @@ namespace MMK_24_3_client
         private void button1_Click(object sender, EventArgs e)
         {
             dataGridView1.Rows.Clear();
+
             Service1Client Client = new Service1Client();
             Point C = new Point((int)Cx.Value, (int)Cy.Value);
             Point D = new Point((int)Dx.Value, (int)Cy.Value);
             float r = (float)C.Y / 2;
+            Rectangle rectangle = new Rectangle(D, C, r);
+            Circle circle = new Circle(r);
+            Triangle triangle = new Triangle(D, C);
+
             if (C.Y > 0 && C.X >= 0 && C.X < D.X)
             {
                 float k = -C.Y / (D.X - C.X);
                 float b = C.Y - k * C.X;
                 float delta = (float)Math.Atan(k);
-                float rectSquare = (D.X + r - C.X) * C.Y;
-                float square = ((D.X - (float)C.X) * C.Y) / 2 + (float)(Math.PI * r * r / 2);
+                float rectSquare = rectangle.Sq;
+                float square = triangle.Sq + (circle.Sq / 2); 
                 RealSquare.Text = square.ToString();
                 for (int i = 0; i < 5; i++)
                 {
@@ -61,11 +66,15 @@ namespace MMK_24_3_client
             Point C = new Point((int)Cx.Value, (int)Cy.Value);
             Point D = new Point((int)Dx.Value, (int)Cy.Value);
             float r = (float)C.Y / 2;
+            Rectangle rectangle = new Rectangle(D, C, r);
+            Circle circle = new Circle(r);
+            Triangle triangle = new Triangle(D, C);
+
                 float k = -C.Y / (D.X - C.X);
                 float b = C.Y - k * C.X;
                 float delta = (float)Math.Atan(k);
-                float rectSquare = (D.X + r - C.X) * C.Y;
-                float square = ((D.X - (float)C.X) * C.Y) / 2 + (float)(Math.PI * r * r / 2);
+                float rectSquare = rectangle.Sq;
+                float square = triangle.Sq + (circle.Sq / 2);
                 RealSquare.Text = square.ToString();
                 for (int i = 0; i < 5; i++)
                 {
@@ -80,4 +89,63 @@ namespace MMK_24_3_client
                 }
         }
     }
+
+    public class Circle
+    {
+        float r;
+        public float Sq;
+        public float Square()
+        {
+            return (float)(Math.PI * this.r * this.r);
+        }
+        public Circle(float ru)
+        {
+            if (ru > 0)
+            {
+                this.r = ru;
+                this.Sq = Square();
+            }
+            else throw new ArgumentException();
+        }
+    }
+
+    public class Rectangle
+    {
+        float R;
+        Point D;
+        Point C;
+        public float Sq;
+        public Rectangle(Point d, Point c, float r)
+        {
+            this.C = c;
+            this.D = d;
+            this.R = r;
+            this.Sq = Square();
+        }
+        float Square()
+        {
+            return (D.X + R - C.X) * C.Y;
+        }
+    }
+
+    public class Triangle
+    {
+        Point D;
+        Point C;
+        public float Sq;
+        public Triangle(Point d, Point c)
+        {
+            this.C = c;
+            this.D = d;
+            this.Sq = Square();
+        }
+
+        private float Square()
+        {
+            return ((D.X - (float)C.X) * C.Y) / 2;
+        }
+    }
+
 }
+
+
